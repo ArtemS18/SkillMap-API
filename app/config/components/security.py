299780470d77b197420b8bf9827492656f553616
Artemiy <artemsikor18@gmail.com>
2026-01-26 19:52:34 +0300

    rename securaty.py->security.py

--- app/config/components/security.py
+++ app/config/components/security.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from config.constants import ENV_FILE_PATH
 
 
-class SecuratyConfig(BaseSettings):
+class SecurityConfig(BaseSettings):
     jwt_access_expires_at: timedelta = timedelta(minutes=30)
     jwt_access_secret_key: str = "secret_key"
     jwt_access_algorithm: str = "HS256"
